0f19adf70abc5c163dc348f40ef0538912e65845
Purple-Code-sh <purple.code.sh@gmail.com>
2023-04-02 18:58:09 -0600

    defining home sections space

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,20 +11,13 @@ function Home() {
         <section className=" flex-shrink-0 h-14 xl:h-16 2xl:h-20 w-full mt-4 lg:mt-2 px-2">
           <Header></Header>
         </section>
-        <section className="flex-auto w-full bg-base">
-          <div className=" h-80 w-full bg-primary">
+        <section className="flex-auto w-ful bg-black mt-4">
+          <div className=" h-96 w-full bg-primary">
             <Categories />
           </div>
-          <div className="h-80 w-full bg-primary-dark">Hola</div>
+          <div className="h-96 w-full bg-primary-dark">Hola</div>
         </section>
       </div>
-      {/* <section className="flex h-5/6 mt-4">
-        
-        <section className=" bg-base-dark w-1/2 h-full">
-          <div className=" bg-primary-light w-full h-1/2">Blog</div>
-          <div className=" bg-primary-dark w-full h-1/2">Xmas</div>
-        </section>
-      </section> */}
     </div>
   );
 }
