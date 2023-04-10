f6314a44f96f9b0e401c0955b43f32e57b445851
Purple-Code-sh <purple.code.sh@gmail.com>
2023-04-10 15:58:15 -0600

    basic carrousel done

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -1,40 +1,75 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import { RxDotFilled } from "react-icons/rx";
 
-//Images import
-import aaaaa from "../assets/aaaaa.jpeg";
-import creaLogo from "../assets/creaLogo.jpeg";
-import elCreaTeNecesita from "../assets/elCreaTeNecesita.jpeg";
+//Images import. Import your own images or it wont work
+import image1 from "../assets/aaaaa.jpeg";
+import image2 from "../assets/creaLogo.jpeg";
+import image3 from "../assets/elCreaTeNecesita.jpeg";
 
 export default function Carrousel() {
-  const imagesArray = [aaaaa, creaLogo, elCreaTeNecesita];
+  const imagesArray = [
+    { image: image1, name: "Guerra de Robots" },
+    { image: image2, name: "Minisumos" },
+    { image: image3, name: "Seguidor de linea" },
+    { image: image1, name: "Futbolito" },
+    { image: image2, name: "Laberinto" },
+    { image: image3, name: "Carrera de insectos" },
+    { image: image1, name: "Drones" },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    let isFirstSlide = currentIndex === 0;
+    let newIndex = isFirstSlide ? imagesArray.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    let isLastSlide = currentIndex === imagesArray.length - 1;
+    let newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (specificSlide) => {
+    setCurrentIndex(specificSlide);
+  };
 
   return (
     <div className="w-full h-full">
       <div className="group bg-base-dark h-4/6 min-w-full m-auto border border-primary/10">
         <div
-          className=" h-full max-w-full bg-cover bg-center duration-500"
-          style={{ backgroundImage: `url(${imagesArray[1]})` }}
+          className=" h-full max-w-full bg-cover bg-center"
+          style={{ backgroundImage: `url(${imagesArray[currentIndex].image})` }}
         />
         <div
           className="opacity-40 group-hover:opacity-100 absolute top-[50%] translate-x-0 translate-y-[-50%] left-2 text-2xl rounded-xl px-2 py-3 bg-black/20 hover:bg-black/40 text-white
          cursor-pointer duration-75  "
         >
-          <BsChevronCompactLeft size={20} />
+          <BsChevronCompactLeft size={20} onClick={prevSlide} />
         </div>
         <div
           className="opacity-40 group-hover:opacity-100 absolute top-[50%] translate-x-0 translate-y-[-50%] right-2 text-2xl rounded-xl px-2 py-3 bg-black/20 hover:bg-black/40 text-white
          cursor-pointer duration-75"
         >
-          <BsChevronCompactRight size={20} />
+          <BsChevronCompactRight size={20} onClick={nextSlide} />
         </div>
       </div>
       <div className=" h-1/6 flex place-items-center justify-center">
-        Categoria
+        {imagesArray[currentIndex].name}
       </div>
       <div className=" h-1/6 flex place-items-center justify-center">
-        Puntitos
+        {imagesArray.map((slide, index) => (
+          <div
+            className=" text-2xl cursor-pointer"
+            key={index}
+            onClick={() => goToSlide(index)}
+          >
+            <RxDotFilled />
+          </div>
+        ))}
       </div>
     </div>
   );
