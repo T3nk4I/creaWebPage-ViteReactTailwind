61b5096c20795825bcb2c97aafbd8bb495783e1b
Purple-Code-sh <purple.code.sh@gmail.com>
2024-09-22 22:45:07 -0600

    fix hamburguer menu display error

--- src/pages/Home/components/Achievements.jsx
+++ src/pages/Home/components/Achievements.jsx
@@ -5,10 +5,11 @@ export default function Achievements () {
   return (
     <div>
       <h2 className=' font-bold text-base sm:text-lg md:text-xl lg:text-2xl  text-primary-dark w-full tracking-wide flex place-items-center justify-center'>
+        <Link to='/creaWebPage-ViteReactTailwind/about' className='cursor-pointer h-full' />
         Nuestros logros
       </h2>
       <div className=' h-40 lg:h-56 2xl:h-64 flex place-content-center pb-4'>
-        <Link to='/creaWebPage-ViteReactTailwind/xmas' className=' cursor-pointer h-full'>
+        <Link to='/creaWebPage-ViteReactTailwind/about' className=' cursor-pointer h-full'>
           <img src={achievements} alt='' className=' h-full' />
         </Link>
       </div>
