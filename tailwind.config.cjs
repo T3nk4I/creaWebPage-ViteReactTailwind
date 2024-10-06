433d502684c370d6dcb0d4fbe54a91b8c7ca4d72
Purple-Code-sh <purple.code.sh@gmail.com>
2024-10-06 02:53:31 -0600

    Defines all colors in the theme. These are the final color selection

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,14 +9,29 @@ module.exports = {
   theme: {
     colors: {
       transparent: 'transparent',
-      white: '#ffffff',
       black: '#000000',
-      primary: '#DB7C00',
-      primary_dark: '#73412F',
-      secondary: '#FEEA00',
-      accent: '#E4007C',
-      light: '#FEFADC',
-      dark: '#380042'
+      white: '#ffffff',
+
+      bg_purple_light: '#400640', // Section boxes.
+      bg_purple: '#380042', // Base background color. **
+      bg_purple_dark: '#380042', // Space, texts, icons.
+
+      bg_accent_light: '#602F35', // Dialog or info boxes.
+      bg_accent: '#591934', // Most important action box or cards.
+      bg_accent_dark: '#4D173B', // Less important action box or cards.
+
+      txt_white: '#FEFADC', // Base text color. **
+      txt_white_purple: '#F2C9F0', // Less important texts.
+
+      primary: '#DB7C00', // Most important button, usually CTAs. **
+      primary_dark: '#8C5A2E', // Less important CTA button and info cards.
+
+      secondary: '#FEEA00', // Highlight, images. (not use too much this color). **
+
+      accent: '#E4007C', // Other buttons (no CTA), icons, highlights. **
+      accent_dark: '#A6037A'// Cards, boxes, less important highlights and icons.
+
+      // The ** are the 5 main colors of the palette.
     },
     extend: {}
   },
